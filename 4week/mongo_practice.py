8e97f92caa14c4fb0479dcfa4cd404cbd058a40e
JaewonKwon <springkjw@gmail.com>
2020-03-29 15:24:56 +0900

    4week

--- 4week/mongo_practice.py
+++ 4week/mongo_practice.py
@@ -9,5 +9,15 @@ def question1():
     )
     print(movie["star"])
 
+def question2():
+    # 어벤져스: 엔드게임 평점이 같은 영화의 제목 가져오기
+    avengers = db.movies.find_one({"title": "어벤져스: 엔드게임"})
+    avengers_star = avengers["star"]
+
+    movies = list(db.movies.find({"star": avengers_star}))
+    for movie in movies:
+        print(movie["title"])
+
 if __name__ == "__main__":
-    question1()
+    # question1()
+    question2()
