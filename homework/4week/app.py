420298e39980971c6ade803d3331189831656e50
JaewonKwon <springkjw@gmail.com>
2020-04-05 13:23:31 +0900

    4week_homework

--- homework/4week/app.py
+++ homework/4week/app.py
@@ -1,10 +1,25 @@
 # app.py
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+from pymongo import MongoClient
+
 app = Flask(__name__)
+client = MongoClient('localhost', 27017)
+db = client.dbsparta
 
 @app.route('/')
 def home():
     return render_template("index.html")
+
+@app.route('/order')
+def order_get():
+    orders = list(db.orders.find())
+    return jsonify({
+        "orders": orders
+    })
+
+@app.route('/order')
+def order_post():
+    return
 
 if __name__ == "__main__":
     app.run('0.0.0.0', port=5000, debug=True)
