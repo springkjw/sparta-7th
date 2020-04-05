cec8cb2cba55c227c08b0ab2495a12adeab56b58
JaewonKwon <springkjw@gmail.com>
2020-04-05 14:44:45 +0900

    5week

--- 5week/memo/app.py
+++ 5week/memo/app.py
@@ -14,8 +14,12 @@ def memo_post():
   comment_received = request.form.get('comment', '')
 
   headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-  data = requests.get(url_receive, headers=headers)
+  data = requests.get(url_received, headers=headers)
   soup = BeautifulSoup(data.text, 'html.parser')
+
+  og_title = soup.select_one('meta[property="og:title"]')
+  og_description = soup.select_one('meta[property="og:description"]')
+  og_image = soup.select_one('meta[property="og:image"]')
 
   return
 
