bef3af4c91ff0e6efdc445640679f9f239dd9004
JaewonKwon <springkjw@gmail.com>
2020-04-05 15:50:13 +0900

    5week

--- 5week/moviestar/app.py
+++ 5week/moviestar/app.py
@@ -18,16 +18,12 @@ def stars_list():
     # 1. mystar 목록 전체를 검색합니다. ID는 제외하고 like 가 많은 순으로 정렬합니다.
     # 참고) find({},{'_id':False}), sort()를 활용하면 굿!
     # 2. 성공하면 success 메시지와 함께 stars_list 목록을 클라이언트에 전달합니다.
-    mystar = list(
-      db.mystar.find({}, {'_id': 0}).sort([("like", DESCENDING)])
-    )
-	return jsonify(
-    {
+    mystar = list(db.mystar.find({}, {'_id': 0}).sort([("like", DESCENDING)]))
+    return jsonify({
       'result': 'success',
       'msg':'list 연결되었습니다!',
       'data': mystar
-    }
-  )
+    })
 
 
 @app.route('/api/like', methods=['POST'])
