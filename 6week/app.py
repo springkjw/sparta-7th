9d75785f26af746f2debe02d55318b2c4d40139e
JaewonKwon <springkjw@gmail.com>
2020-04-12 13:23:34 +0900

    6week

--- 6week/app.py
+++ 6week/app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+from pymongo import MongoClient
 
 app = Flask(__name__)
+client = MongoClient('localhost', 27017)
+db = client.dbsparta
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/app/list', methods=["GET"])
+def movie_star_list():
+    mystar = list(db.mystar.find({}, {'_id', 0}))
+    return jsonify(mystar)
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
